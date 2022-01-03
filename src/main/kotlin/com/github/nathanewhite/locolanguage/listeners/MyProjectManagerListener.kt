da65fec5731ac8ddb62b45d9eccdab0228e7c27b
GitHub Action <action@github.com>
2022-01-03 02:56:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/nathanewhite/locolanguage/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nathanewhite/locolanguage/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nathanewhite.locolanguage.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nathanewhite.locolanguage.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/nathanewhite/locolanguage/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nathanewhite/locolanguage/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nathanewhite.locolanguage.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nathanewhite.locolanguage.MyBundle
 
 class MyApplicationService {
 
